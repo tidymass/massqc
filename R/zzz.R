f179eaea5587726a4024554b24feb2d325a346a7
jaspershen <shenxt1990@outlook.com>
2021-12-22 23:48:06 -0800

    modify code

--- R/zzz.R
+++ R/zzz.R
@@ -5,57 +5,16 @@
   
   crayon::num_colors(TRUE)
   massqc_attach()
+  
+  if (!"package:conflicted" %in% search()) {
+    x <- massqc_conflicts()
+    msg(massqc_conflict_message(x), startup = TRUE)
+  }
+  
 }
 
 is_attached <- function(x) {
   paste0("package:", x) %in% search()
 }
 
-
-globalVariables(names = c(
-  "silence.deprecated",
-  "name",
-  "injection.order",
-  "peak.name",
-  "sample.name",
-  "min.fraction.qc",
-  "batch",
-  "median",
-  "maxo",
-  "sample_name",
-  "intensity",
-  "rt",
-  "rtmed",
-  "mzmed",
-  "x",
-  "freq",
-  "xdata3",
-  "group",
-  "mz",
-  "colou.index",
-  "X1",
-  "X2",
-  "Class",
-  "value",
-  "sample_group",
-  "rt.min",
-  "rt.max",
-  "min.intensity",
-  "max.intensity",
-  "diffRT",
-  "data.bpca",
-  "int",
-  "query.RT",
-  "reference.RT",
-  "Degree.Span.MSE",
-  "Poly.MSE",
-  "extenstion",
-  "rt_correction",
-  "variable_id",
-  "value",
-  "sample_id",
-  "na_freq",
-  "PC1",
-  "rsd"
-))
 
